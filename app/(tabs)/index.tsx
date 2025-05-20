1d4ad6239cd7e03019dfc472ff34870fc4d26743
BrunoPereiradeSouza <brunosouzaff10@gmail.com>
2025-05-20 20:51:11 -0300

    feat: improve contador design

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,75 +1,124 @@
-import { Image } from 'expo-image';
-import { Platform, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { Pressable, StyleSheet, Text, View } from 'react-native';
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+export default function ContadorSimples() {
+  // Estado para armazenar o valor do contador
+  const [contador, setContador] = useState(0);
+  const [mostrarMsg, setMostrarMsg] = useState(false);
 
-export default function HomeScreen() {
+  // Funções para manipular o contador usando declaração de função normal
+  function aumentar() {
+    setContador(contador + 1);
+  }
+
+  function diminuir() {
+    setContador(contador - 1);
+  }
+
+  function resetar() {
+    setContador(0);
+    setMostrarMsg(true);
+
+
+    setTimeout(() => {
+    setMostrarMsg(false);
+  }, 2000);
+  }
+
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12',
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          {`Tap the Explore tab to learn more about what's included in this starter app.`}
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          {`When you're ready, run `}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+    <View style={styles.container}>
+      {/* Título do App */}
+      <Text style={styles.titulo}>Contador App</Text>
+
+      {/* Valor do Contador */}
+      <View style={styles.contador}>
+        <Text style={styles.contadorText}>{contador}</Text>
+      </View>
+
+      {mostrarMsg && (
+        <Text style={[styles.resetText, styles.colorGray]}>Contador reiniciado!</Text>
+      )}
+
+      {/* Botões de Controle - Usando o componente Button padrão */}
+      <View style={styles.botoes}>
+        <Pressable style={[styles.btn, styles.backgroundRed]} onPress={diminuir}>
+          <Text style={styles.btnText}>-</Text>
+        </Pressable>
+        <Pressable style={[styles.btn, styles.backgroundGray]} onPress={resetar}>
+          <Text style={styles.btnText}>Reset</Text>
+        </Pressable>
+        <Pressable style={[styles.btn, styles.backgroundBlue]} onPress={aumentar}>
+          <Text style={styles.btnText}>+</Text>
+        </Pressable>
+      </View>
+      <View style={styles.expoContainer}>
+        <Text style={styles.colorGray}>Aprenda React Native com Expo!</Text>
+      </View>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
+  container: {
+    flex: 1,
     alignItems: 'center',
-    gap: 8,
+    justifyContent: 'center',
+    backgroundColor: 'whitesmoke',
   },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
+  titulo: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    marginBottom: 40,
   },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
+  contador: {
+    marginBottom: 30,
+    width: 120,
+    height: 120,
+    elevation: 5,
+    borderRadius: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'white',
+  },
+  contadorText: {
+    fontSize: 40,
+    fontWeight: 'bold',
+  },
+  resetText: {
+    marginTop: 5,
+    marginBottom: 30,
+  },
+  botoes: {
+    flexDirection: 'row',
+    width: '80%',
+    color: 'white',
+    justifyContent: 'space-between',
+  },
+  btn: {
+    borderRadius: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: 70,
+    width: 70, 
+  },
+  btnText: {
+    color: 'white',
+    fontSize: 20,
+  },
+  backgroundRed: {
+    backgroundColor: 'red',
+  },
+  backgroundGray: {
+    backgroundColor: 'gray',
+  },
+  backgroundBlue: {
+    backgroundColor: 'blue',
+  },
+  expoContainer: {
     position: 'absolute',
+    bottom: 20,
+  },
+  colorGray: {
+    color: "gray",
   },
 });
